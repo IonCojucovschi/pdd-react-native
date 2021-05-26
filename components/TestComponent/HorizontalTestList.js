8ea295631141018ca880954928442ed06b1e631b
ioncojucovschii <ion.cojucovschi@jivygroup.com>
2021-05-27 01:09:31 +0300

    Validate view with selection logic

---
+++ components/TestComponent/HorizontalTestList.js
@@ -0,0 +1,78 @@
+import React, { Component,useState,useEffect } from "react";
+import { AppRegistry, View, Text, StyleSheet, Image, Button, FlatList, TouchableHighlight } from "react-native";
+
+const stylesList = StyleSheet.create({
+    container: {
+      //flex: 1,
+      //paddingTop: 22,
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+    textStyle:{
+      fontSize:15,
+      color:"#000000",
+      alignSelf:"center",
+      //backgroundColor:"#d1c541"
+    },
+    itemList:{
+      color:'#cdcdcd',
+      backgroundColor:'#06ba1e',
+      padding:5,
+      width:80,
+      height:50,
+      flex:1,
+      borderRadius:4,
+      alignItems:"space-between",
+      margin: 5
+    }
+})
+
+class  HorizontalTestList extends Component  {
+    constructor(props){
+        super(props);
+        console.log("Tests: ",this.props);
+    }
+    ///console.log("All Tests: ",state);
+
+    goToSenectedTest=(test)=>{
+      //navigation.navigate('Home', { id: test});
+      console.log("Selected test is ", test);
+    }
+
+   render(){ 
+        return <View style={stylesList.container}>
+                <FlatList
+                    horizontal={true}
+                    keyExtractor={(item1)=>"elm0"+item1.Id}
+                    data={ this.props.allTests }
+                    style={{backgroundColor:"#eee"}}
+                    renderItem={({ item, index }) => (<TouchableHighlight 
+                    style={
+                        {
+                        color:'#cdcdcd',
+                        backgroundColor: (item.backgroundColor ? item.backgroundColor :'#ddd'),///'#06ba1e',
+                        padding:5,
+                        width:80,
+                        height:50,
+                        flex:1,
+                        borderRadius:4,
+                        alignItems:"space-between",
+                        margin: 5
+                        }
+                    } 
+                    onPress={()=>this.goToSenectedTest(item.Id)}>
+                        <View>
+                            <View >
+                            <Text style={stylesList.textStyle}>{"Test " + index }</Text>
+                            </View>
+                        </View>
+                        </TouchableHighlight>)}
+                    />
+            </View>
+    }
+}
+
+export default HorizontalTestList;
--- screens/SelectedTestScreen.js
+++ screens/SelectedTestScreen.js
@@ -1,87 +1,93 @@
-import React from "react";
+import React, { Component,useState,useEffect } from "react";
 import { AppRegistry, View, Text, StyleSheet, Image, Button, FlatList, TouchableHighlight } from "react-native";
 import testResources from "../components/testscreen/testList.json";
 import AllTests from "../AllTest.json";
+import HorizontalTestList from '../components/TestComponent/HorizontalTestList';
 
 
+class SelectedTestScreen extends Component {
+
+    constructor(props){
+      super(props);
+      let allTest = AllTests.filter(elm=>elm.ChapterKey=="chapter1"); 
 
 
+      ///let test = AllTests[0]; 
+      ///allTest.Responses = allTest.Responses.map(itm=>{itm.backgroundColor="#ccc"; return itm;}); 
+      this.state={  test:allTest[0], allTests:allTest  };
 
-const stylesList = StyleSheet.create({
-    container: {
-      flex: 1,
-      //paddingTop: 22,
-    },
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
-    },
-    textStyle:{
-      fontSize:15,
-      color:"#000000",
-      alignSelf:"center",
-      //backgroundColor:"#d1c541"
-    },
-    itemList:{
-      color:'#cdcdcd',
-      backgroundColor:'#06ba1e',
-      padding:5,
-      width:80,
-      height:50,
-      flex:1,
-      borderRadius:4,
-      alignItems:"space-between",
-      margin: 5
-    }
-})
 
-const TestList = () => {
-
-    const goToSenectedTest=(test)=>{
-      //navigation.navigate('Home', { id: test});
-      console.log("Selected test is ", test);
     }
 
-    return <View style={stylesList.container}>
-    <FlatList
-        horizontal={true}
-        data={testResources}
-        style={{backgroundColor:"#dedede"}}
-        renderItem={({ item }) => (<TouchableHighlight style={stylesList.itemList} onPress={()=>goToSenectedTest(item.id)}>
-              <View>
-                <View >
-                  <Text style={stylesList.textStyle}>{item.name}</Text>
-                </View>
+    responseClick=(rs)=>{
+      let tempTest=this.state;
+
+      tempTest.test.Responses = this.state.test.Responses.map((el)=>{
+        el.backgroundColor="#f22416";
+        if(el.IsRight)  
+          el.backgroundColor="#06c20f";
+
+        return el;  
+      });
+      tempTest.test.backgroundColor="#f22416";
+      if(rs.IsRight){
+        tempTest.test.backgroundColor="#06c20f";
+      }
+      //select test wich
+      let nextTest = tempTest.allTests.find(e=>!e.backgroundColor);
+      
+      tempTest.allTests=tempTest.allTests.map(elm=>{
+        if(rs.IsRight && elm.Id==tempTest.test.Id)
+          return tempTest.test;
+        else
+          return elm; 
+      });
+      tempTest.test = nextTest ? nextTest :{ Responses:[]};
+      console.log("Rs clicked1 :",tempTest);
+      
+      
+      
+      this.setState(tempTest);
+      //setUpdatedValues(tempTest);
+    }
+   render(){
+    return (<View styles={{ flex:1, height:'800', flexDirection:'column', backgroundColor:"#008000"}}>
+              <View style={{flex:1}}>
+                 <HorizontalTestList allTests={this.state.allTests} />
               </View>
-          </TouchableHighlight>)}
-    />
-</View>
-}
+              <View style={{flex:3}}>
+                {this.state.test.ImageUri ?
+                  <Image style={{height:200,width:null, flex:1, backgroundColor:'#ccc', alignContent:"center"}}
+                      source={require("../img"+ this.state.test.ImageUri)}
+                  />:null
+                }
+              </View>
+              <View style={{flex:6, alignContent:'space-between'}}>
+                {this.state.test.Responses.map(el=>
+                   <View key={"str"+el.Id} style={{margin:10}}>
+                     <Button
+                      onPress={()=>this.responseClick(el)}
+                      color={el.backgroundColor ? el.backgroundColor :"#ccc" }
+                      title={el.Response}
+                      margin="5"
+                     />
+                   </View>)}
+              </View>
 
-
-
-
-const SelectedTestScreen = ({navigation}) =>{
-    ///<Button title="Menu Lateral"  onPress={toggleMenu} />
-            
-    return  (<View styles={styles.container}>
-                <TestList navigation={navigation}/>
-                <View>
-                    <Image style={{height:200,width:null, flex:1, backgroundColor:'#ccc', alignContent:"center"}}
-                        source={require("../img"+AllTests[0].ImageUri)}
-                    />
-                </View>
-                
-            </View>)
+          </View>)
+   }      
 }
     
 
 const styles = StyleSheet.create({
     container:{
-        //flex:1,
-        alignItems:"center",
-        justifyContent:"center"
+        flex:1,
+        flexDirection:'column',
+        padding:5,
+        backgroundColor:'#000',
+        flexWrap:'wrap',
+        //alignItems:"center",
+        //justifyContent:"center"
     }
 })
 
